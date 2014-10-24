2b229638174a45bda1f214f60ff9454aea76994c
linsonglnkd <sl7h.tech@gmail.com>
2014-10-24 14:29:36 -0700

    Update stringToArray.java

--- Java/stringUDF/stringToArray.java
+++ Java/stringUDF/stringToArray.java
@@ -14,22 +14,13 @@ import org.apache.pig.impl.logicalLayer.schema.Schema;
 public class stringToArray extends EvalFunc<DataBag>
 {	
 	public DataBag exec(Tuple input) throws IOException {
-		//if (input == null || input.size() == 0) return null;
+		if (input == null || input.size() == 0) return null;
 		DataBag outBag = BagFactory.getInstance().newDefaultBag();
-
 		try{
-			if (input == null || input.size() == 0) {
-				String myStr = null;
-				Tuple myTuple = TupleFactory.getInstance().newTuple(myStr);
-				outBag.add(myTuple);
-				return outBag;
-			}
-			else {
-				String myStr = (String) input.get(0);
-				Tuple myTuple = TupleFactory.getInstance().newTuple(myStr);
-				outBag.add(myTuple);
-				return outBag;
-			}
+			String myStr = (String) input.get(0);
+			Tuple myTuple = TupleFactory.getInstance().newTuple(myStr);
+			outBag.add(myTuple);
+			return outBag;
 		}catch(Exception e){
 			throw new IOException("Caught exception processing input row ", e);
 		}
@@ -38,10 +29,10 @@ public class stringToArray extends EvalFunc<DataBag>
 	public Schema outputSchema(Schema input) {
 		try {
 			Schema stringSchema = new Schema();
-	        stringSchema.add(new Schema.FieldSchema("ARRAY_ELEM", DataType.CHARARRAY));
-	        Schema bagSchema = new Schema(stringSchema);
-	        Schema.FieldSchema bagFs = new Schema.FieldSchema("PIG_WRAPPER",bagSchema, DataType.BAG);
-	        return new Schema(bagFs);        
+	        	stringSchema.add(new Schema.FieldSchema("ARRAY_ELEM", DataType.CHARARRAY));
+	        	Schema bagSchema = new Schema(stringSchema);
+	        	Schema.FieldSchema bagFs = new Schema.FieldSchema("PIG_WRAPPER",bagSchema, DataType.BAG);
+	        	return new Schema(bagFs);        
 		}catch(Exception e){
 			return null;
 		}
